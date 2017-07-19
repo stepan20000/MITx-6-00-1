3cb7736978b0b418f5c09fcb963f95456c425605
Stepan Yefimov <stepan.yefimov@gmail.com>
2017-07-19 07:17:30 +0300

    Pset5 done

--- test.py
+++ test.py
@@ -1,28 +1,10 @@
-def is_list_permutation(L1, L2):
-    if len(L1) != len(L2):
-        return False
-    else:
-        if len(L1) == 0 and len(L2) == 0:
-            return(None, None, None)
-    count = 0
-    element = L1[0]
-    for el in L1:
-        tempCount = L1.count(el)
-        if tempCount != L2.count(el):
-            return False
-        else:
-            if tempCount > count:
-                element = el
-                count = tempCount
-    return (element, count, type(element))
-    
-        
-    
-    
-    
+import string
 
-L1 = [] 
-L2 = []
-print(is_list_permutation(L1, L2))
-        
-
+def foo(shift):
+    shiftDict = {}
+    for l in string.ascii_lowercase:
+        shiftDict[l] = chr((ord(l) - 97 + shift)%26 + 97)
+    for l in string.ascii_uppercase:
+        shiftDict[l] = chr((ord(l) - 65 + shift)%26 + 65)
+    return shiftDict
+print(foo(1))
